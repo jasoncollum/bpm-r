b14a78ed194870c145984ced9db576a442b5875b
Jason Collum <jasoncollum@gmail.com>
2019-11-02 13:30:18 -0500

    add HomePage and Branding components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
 
+import HomePage from './pages/homepage/homepage.component';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          bpm
-        </p>
-        <div className="icon-div">
-          <i className="fas fa-heartbeat heart icon"></i>
-          <i className="fas fa-heartbeat heartbeat icon"></i>
-          <i className="fas fa-weight weight icon"></i>
-          <i className="fas fa-sticky-note note icon"></i>
-        </div>
-      </header>
+      <HomePage />
     </div>
   );
 }
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Branding from '../../components/branding/branding.component';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className="homepage">
+        <Branding />
+        <p className="tag-line">monitor your blood pressure</p>
+    </div>
+);
+
+export default HomePage;
