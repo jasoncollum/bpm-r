5a320487ccabe611deae2726f238ef0cd5234865
Jason Collum <jasoncollum@gmail.com>
2019-11-02 18:30:23 -0500

    initial setup for sign-in form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component';
 
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Switch>
         <Route exact path={'/'} component={HomePage} />
+        <Route path={'/signin'} component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
--- src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component.jsx
+++ src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-// import SignIn from '../../components/sign-in/sign-in.component';
+import SignIn from '../../components/sign-in/sign-in.component';
 // import SignUp from '../../components/sign-up/sign-up.component';
 
-import './sign-in-sign-up.styles.scss';
+import './sign-in-and-sign-up-page.styles.scss';
 
 const SignInAndSignUpPage = () => (
     <div className='sign-in-and-sign-up'>
-        {/* <SignIn />
-        <SignUp /> */}
+        <SignIn />
+        {/* <SignUp /> */}
     </div>
 );
 
