45afdc6bd9cc0edf501dd56fcec87d7cd32ac4c5
Jason Collum <jasoncollum@gmail.com>
2019-11-12 10:19:45 -0600

    add entries.map to return React.Fragment for each entry

--- src/components/entries/entries.component.jsx
+++ src/components/entries/entries.component.jsx
@@ -3,6 +3,27 @@ import { Table } from 'reactstrap';
 
 import './entries.styles.scss';
 
+const entries = [
+    {
+        id: 1,
+        date: '11/10/2019 3:57 PM',
+        systolic: 116,
+        diastolic: 78,
+        pulse: 68,
+        weight: 140,
+        notes: 'Took normal morning meds and walked three miles.'
+    },
+    {
+        id: 2,
+        date: '11/11/2019 2:16 PM',
+        systolic: 121,
+        diastolic: 83,
+        pulse: 74,
+        weight: 140,
+        notes: 'Took normal morning meds.'
+    }
+]
+
 const Entries = () => (
     <div className='entries-container'>
         <Table className='table'>
@@ -16,30 +37,24 @@ const Entries = () => (
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>11/10/2019 3:57 PM</td>
-                    <td>116/78</td>
-                    <td>68</td>
-                    <td>140</td>
-                    <td><i class='fas fa-pencil-alt pencil-icon'></i></td>
-                </tr>
-                <tr>
-                    <td className='table-note' colspan="12">
-                        Took normal morning meds and walked three miles.
-                    </td>
-                </tr>
-                <tr>
-                    <td>11/10/2019 3:57 PM</td>
-                    <td>116/78</td>
-                    <td>68</td>
-                    <td>140</td>
-                    <td><i class='fas fa-pencil-alt pencil-icon'></i></td>
-                </tr>
-                <tr>
-                    <td className='table-note' colspan="12">
-                        Took normal morning meds.
-                    </td>
-                </tr>
+                {
+                    entries.map(({ id, date, systolic, diastolic, pulse, weight, notes }) => (
+                        <React.Fragment key={id}>
+                            <tr>
+                                <td>{date}</td>
+                                <td>{systolic}/{diastolic}</td>
+                                <td>{pulse}</td>
+                                <td>{weight}</td>
+                                <td><i class='fas fa-pencil-alt pencil-icon'></i></td>
+                            </tr>
+                            <tr>
+                                <td className='table-note' colspan="12">{notes}
+                                </td>
+                            </tr>
+                        </React.Fragment>
+                    ))
+                }
+
             </tbody>
         </Table>
     </div>
