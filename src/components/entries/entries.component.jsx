cc152bf0e090047c2cfceecd4912c30dc0203ad6
Jason Collum <jasoncollum@gmail.com>
2020-01-10 10:48:03 -0600

    add useEffect back fetch entries and render entries

--- src/components/entries/entries.component.jsx
+++ src/components/entries/entries.component.jsx
@@ -1,51 +1,25 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Table } from 'reactstrap';
-// import { connect } from 'react-redux';
-// import { createStructuredSelector } from 'reselect';
 
 import { firestore } from '../../firebase/firebase.utils';
-// import { selectCurrentUser } from '../../redux/user/user.selectors';
+
 import CurrentUserContext from '../../contexts/current-user.context';
 import './entries.styles.scss';
 
-// const entries = [
-//     {
-//         id: 1,
-//         date: '11/10/2019 3:57 PM',
-//         systolic: 116,
-//         diastolic: 78,
-//         pulse: 68,
-//         weight: 140,
-//         notes: 'Took normal morning meds and walked three miles.'
-//     },
-//     {
-//         id: 2,
-//         date: '11/11/2019 2:16 PM',
-//         systolic: 121,
-//         diastolic: 83,
-//         pulse: 74,
-//         weight: 140,
-//         notes: 'Took normal morning meds.'
-//     }
-// ]
-
-const Entries = (props) => {
+const Entries = () => {
     const [entries, setEntries] = useState([]);
     const currentUser = useContext(CurrentUserContext);
     console.log("ENTRIES-USER::", currentUser);
-    // const activeUser = props.user;
 
-    // useEffect(() => {
-    //     const fetchData = async (userId) => {
-    //         const data = await firestore.collection(`users/${userId}/entries`)
-    //             .orderBy("date", "asc")
-    //             .get();
-    //         setEntries(data.docs.map(doc => doc.data()));
-    //     }
-    //     fetchData(activeUser.id);
-    // }, [])
-
-    // const filteredEntries = entries.filter(entry => entry.date > "12/1/2019 2:00 PM");
+    useEffect(() => {
+        const fetchData = async (userId) => {
+            const data = await firestore.collection(`users/${userId}/entries`)
+                .orderBy("date", "asc")
+                .get();
+            setEntries(data.docs.map(doc => doc.data()));
+        }
+        fetchData(currentUser.id);
+    }, [])
 
     return (
         <div className="entries-container">
@@ -83,12 +57,5 @@ const Entries = (props) => {
         </div>
     )
 }
-
-
-// const mapStateToProps = createStructuredSelector({
-//     currentUser: selectCurrentUser
-// });
-
-// export default connect(mapStateToProps)(Entries);
 
 export default Entries;
