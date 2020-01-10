4970408a21875c251fbf38428663185faf2bccea
Jason Collum <jasoncollum@gmail.com>
2020-01-10 17:43:30 -0600

    add greeting and signin signout to homepage

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { auth } from '../../firebase/firebase.utils';
+
+import CurrentUserContext from '../../contexts/current-user.context';
 
 import Branding from '../../components/branding/branding.component';
 
 import './homepage.styles.scss';
 
-const HomePage = () => (
-    <div className='homepage'>
-        <Branding />
-        <p className='tag-line'>monitor your blood pressure</p>
-    </div>
-);
+const HomePage = () => {
+    const currentUser = useContext(CurrentUserContext)
+
+    return (
+        <div className='homepage'>
+            <Branding />
+            <p className='tag-line'>monitor your blood pressure</p>
+            {currentUser ? (
+                <>
+                    <h3 className='homepage-greeting'>Hello, {currentUser.displayName}</h3>
+                    <span className='homepage-option'
+                        onClick={() => auth.signOut()}>
+                        SIGN OUT
+                    </span>
+                </>
+            ) : (
+                    <Link className='homepage-option'>SIGN IN</Link>
+                )
+            }
+
+        </div>
+    )
+};
 
 export default HomePage;
