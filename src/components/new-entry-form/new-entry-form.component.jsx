5fc7bc27060507a65898bad41621ec91c205023c
Jason Collum <jasoncollum@gmail.com>
2020-01-13 13:20:29 -0600

    add new-entry-form styling

--- src/components/new-entry-form/new-entry-form.component.jsx
+++ src/components/new-entry-form/new-entry-form.component.jsx
@@ -1,19 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './new-entry-form.styles.scss';
 
 const NewEntryForm = () => {
-    // const [newEntryData, setNewEntryData] = useState({
-    //     date: '',
-    //     systolic: null,
-    //     diastolic: null,
-    //     pulse: null,
-    //     weight: null,
-    //     notes: ''
-    // });
+    const [newEntryData, setNewEntryData] = useState({
+        date: '',
+        systolic: '',
+        diastolic: '',
+        pulse: '',
+        weight: '',
+        notes: ''
+    });
+
+    const { date, systolic, diastolic, pulse, weight, notes } = newEntryData;
+
+    const handleSubmit = e => {
+        console.log('New Entry Form SUBMIT Button Clicked...');
+    }
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        console.log('New Entry Form HANDLE_CHANGE', name, value);
+        // setNewEntryData({ ...newEntryData, [name]: value });
+    }
 
     return (
-        <h1>NEW ENTRY FORM</h1>
+        <div className='new-entry-form'>
+            {/* <span>Add a new entry</span> */}
+            <form className='sign-up-form' onSubmit={handleSubmit}>
+                <FormInput
+                    type='text'
+                    name='systolic'
+                    value={systolic}
+                    onChange={handleChange}
+                    label='systolic'
+                    required
+                />
+                <FormInput
+                    type='text'
+                    name='diastolic'
+                    value={diastolic}
+                    onChange={handleChange}
+                    label='diastolic'
+                    required
+                />
+                <FormInput
+                    type='text'
+                    name='pulse'
+                    value={pulse}
+                    onChange={handleChange}
+                    label='pulse'
+                    required
+                />
+                <FormInput
+                    type='text'
+                    name='weight'
+                    value={weight}
+                    onChange={handleChange}
+                    label='weight'
+                    required
+                />
+                <textarea
+                    className='entry-form-textarea'
+                    name='notes'
+                    value={notes}
+                    placeholder='Enter notes here...'
+                    onChange={handleChange}
+                    rows='2'
+                >
+
+                </textarea>
+                <CustomButton type='submit'> SAVE ENTRY </CustomButton>
+            </form>
+
+        </div>
     )
 }
 
