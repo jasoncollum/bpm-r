fe0375025a73491412cfb2366ce12cb8596352bc
Jason Collum <jasoncollum@gmail.com>
2020-01-19 19:45:00 -0600

    refactor App.js to functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Header from './components/header/header.component';
@@ -15,111 +15,75 @@ import CurrentUserContext from './contexts/current-user.context';
 
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [currentUser, setCurrentUser] = useState(null);
 
-    this.state = {
-      currentUser: null
-    };
-
-    this.signUserOut = this.signUserOut.bind(this);
-  }
-
-  signUserOut() {
+  const signUserOut = () => {
     auth.signOut();
-    localStorage.removeItem('currentUser');
-    this.setState({ currentUser: null });
+    setCurrentUser(null);
   }
 
-  unsubscribeFromAuth = null;
+  auth.onAuthStateChanged(async userAuth => {
+    if (userAuth) {
+      const userRef = await createUserProfileDocument(userAuth);
 
-  componentWillMount() {
-    localStorage.getItem('currentUser') && this.setState({
-      currentUser: JSON.parse(localStorage.getItem('currentUser'))
-    })
-  }
-
-  componentDidMount() {
-    if (!localStorage.getItem('currentUser')) {
-      this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-        if (userAuth) {
-          const userRef = await createUserProfileDocument(userAuth);
-
-          userRef.onSnapshot(snapShot => {
-            this.setState({
-              currentUser: {
-                id: snapShot.id,
-                ...snapShot.data()
-              }
-            });
-          });
-        }
-
-        this.setState(userAuth);
-
+      userRef.onSnapshot(snapShot => {
+        setCurrentUser(
+          {
+            id: snapShot.id,
+            ...snapShot.data()
+          }
+        )
       });
-    } else {
-      console.log('Using data from localStorage')
     }
-  }
+  });
 
-  componentWillUpdate(nextProps, nextState) {
-    localStorage.setItem('currentUser', JSON.stringify(nextState.currentUser));
-  }
+  return (
+    <div className="App" >
+      <CurrentUserContext.Provider value={currentUser}>
+        <Header />
 
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
-
-  render() {
-    return (
-      <div className="App" >
-        <CurrentUserContext.Provider value={this.state.currentUser}>
-          <Header />
-
-          <Switch>
-            <Route exact path='/'
-              render={() => <HomePage signUserOut={this.signUserOut} />}
-            />
-            <Route exact path='/entries'
-              render={() =>
-                !this.state.currentUser ? (
-                  <Redirect to='/signin' />
-                ) : (
-                    <Entries />
-                  )}
-            />
-            <Route exact path='/newentryform'
-              render={() =>
-                !this.state.currentUser ? (
-                  <Redirect to='/signin' />
-                ) : (
-                    <NewEntryForm />
-                  )}
-            />
-            <Route
-              exact path='/signin'
-              render={() =>
-                this.state.currentUser ? (
-                  <Redirect to='/entries' />
-                ) : (
-                    <SignIn />
-                  )}
-            />
-            <Route path='/signup'
-              render={() =>
-                this.state.currentUser ? (
-                  <Redirect to='/entries' />
-                ) : (
-                    <SignUp />
-                  )}
-            />
-          </Switch>
-        </CurrentUserContext.Provider>
-      </div>
-    );
-  }
+        <Switch>
+          <Route exact path='/'
+            render={() => <HomePage signUserOut={signUserOut} />}
+          />
+          <Route exact path='/entries'
+            render={() =>
+              !currentUser ? (
+                <Redirect to='/signin' />
+              ) : (
+                  <Entries />
+                )}
+          />
+          <Route exact path='/newentryform'
+            render={() =>
+              !currentUser ? (
+                <Redirect to='/signin' />
+              ) : (
+                  <NewEntryForm />
+                )}
+          />
+          <Route
+            exact path='/signin'
+            render={() =>
+              currentUser ? (
+                <Redirect to='/entries' />
+              ) : (
+                  <SignIn />
+                )}
+          />
+          <Route path='/signup'
+            render={() =>
+              currentUser ? (
+                <Redirect to='/entries' />
+              ) : (
+                  <SignUp />
+                )}
+          />
+        </Switch>
+      </CurrentUserContext.Provider>
+    </div>
+  );
 }
 
 export default App;
