67c63e39acc62e3c32e10c22582dc1d6aa2f3ebd
Jason Collum <jasoncollum@gmail.com>
2020-06-28 12:50:58 -0500

    Update firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-    apiKey: "AIzaSyBiQglNYlSQfFcpd6wgN4CFg719KLCzhe4",
-    authDomain: "bpm-r-979af.firebaseapp.com",
-    databaseURL: "https://bpm-r-979af.firebaseio.com",
-    projectId: "bpm-r-979af",
-    storageBucket: "bpm-r-979af.appspot.com",
-    messagingSenderId: "734170694734",
-    appId: "1:734170694734:web:4b67eb7007359e85c7ef1f"
+// const config = {
+//    apiKey: 
+//    authDomain: 
+//    databaseURL: 
+//    projectId: 
+//    storageBucket: 
+//    messagingSenderId: 
+//    appId: 
 };
 
 firebase.initializeApp(config);
